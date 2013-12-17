7427ac7c823abd18df23739496f317c9a81bd36a
egucciar <egucciar@gmail.com>
2013-12-16 17:51:38 -0800

    update to css

---
+++ Scalejs.ProjectTemplate.MvvmApplication.Css/Scripts/_references.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="../js/$projectname$.js" />
+/// <reference path="~/Scripts/_scalejs.intellisense.js" />
---
+++ Scalejs.ProjectTemplate.MvvmApplication.Less/config.js
@@ -0,0 +1,22 @@
+var require = {
+    "baseUrl":  ".",
+    "config": {
+        "scalejs.statechart-scion": {
+            "logStatesEnteredAndExited": true
+        }
+    },
+    "paths": {
+        "jasmine":  "Scripts/jasmine",
+        "jasmine-html":  "Scripts/jasmine-html"
+    },
+    "shim": {
+        "jasmine":  {
+            "exports":  "jasmine"
+        },
+        "jasmine-html":  {
+            "deps":  [
+                "jasmine"
+            ]
+        }
+    }
+};
